fe55ba17f38a3b8bb242652b3a32d12054468edb
Muhammad Rana <muhammadrana523@gmail.com>
2022-01-03 15:45:21 +0500

    Changes

--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -32,6 +32,7 @@
                                     <th width="20%">Name</th>
                                     <th width="15%">Phone</th>
                                     <th width="20%">Email</th>
+                                    <th>Care Plan</th>
                                     <th>Address</th>
                                     <th width="3%">Modify</th>
                                     <th width="3%">Delete</th>
@@ -44,6 +45,7 @@
                                         <td>{{ $patient->name}}</td>
                                         <td>{{ $patient->phone }}</td>
                                         <td>{{ $patient->email }}</td>
+                                        <td><a target="_blank" href="{{ asset($patient->care_plan) }}"><span class="badge badge-info">Show</span></a></td>
                                         <td>{{ $patient->address }}</td>
                                         <td><a href="{{ route('patient.edit', ['patient' => $patient]) }}"><i class="fa fa-edit"></i></a></td>
                                         <td><button value="{{ $patient->id }}" style="border: none; background-color: #fff" class="delete_btn"><i class="fa fa-trash"></i></button></td>
