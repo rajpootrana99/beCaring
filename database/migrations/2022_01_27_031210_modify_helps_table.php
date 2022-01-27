6fafa806268abdf63f760be34213293bb936d050
Muhammad Rana <muhammadrana523@gmail.com>
2022-01-27 08:13:50 +0500

    Chnages

---
+++ database/migrations/2022_01_27_031210_modify_helps_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyHelpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('helps', function (Blueprint $table){
+            $table->longText('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
